136142c946b6f67350793bd3b17098adc1a4bc06
Mizanur Rahman <mizanur.rahman@navana.com>
2022-06-11 14:03:49 +0600

    testing parameter of a class

--- src/Food.spec.ts
+++ src/Food.spec.ts
@@ -4,8 +4,10 @@ import Food from './Food';
 
 describe('Food', () => {
   test('create', () => {
-    const food = new Food();
+    const food = new Food('rice', 'g');
 
     expect(food).toBeDefined();
+    expect(food.getName()).toEqual('rice');
+    expect(food.getUnit()).toEqual('g');
   });
 });
--- src/Food.ts
+++ src/Food.ts
@@ -1,4 +1,14 @@
 /** @format */
-class Food {}
+class Food {
+  constructor(private readonly name: string, private readonly unit: string) {}
+
+  getName(): string {
+    return this.name;
+  }
+
+  getUnit(): string {
+    return this.unit;
+  }
+}
 
 export default Food;
